214f9044554187d9e6f4bf2648abc2c006c66f3a
42arch <rend42@163.com>
2023-05-19 01:38:46 +0800

    feat(07): add blob

--- pages/07/main.js
+++ pages/07/main.js
@@ -1,80 +1,61 @@
 import * as d3 from 'd3'
 
-const createLineChart = async () => {
-  const id = '#chart1'
-  const data = await d3.json('../data/washington_dc_weather.json')
+class Blob {
+  constructor(id, color) {
+    this.id = id
+    this.color = color
+    this.margin = { top: 20, right: 30, bottom: 20, left: 30 }
+    this.data = [
+      5.447060485384432, 4.971556756831614, 5.356425214200197,
+      5.224937072128102, 5.2225645937212475, 4.70468448669522,
+      3.5719921252670392, 5.5701623121438315
+    ]
+    this.width = d3.select(id).node().offsetWidth
+    this.height = d3.select(id).node().offsetHeight
+    this.boundWidth = this.width - this.margin.right - this.margin.left
+    this.boundHeight = this.height - this.margin.top - this.margin.bottom
+    this.wrapper = null
+    this.content = null
+    this.path = null
 
-  const width = d3.select(id).node().offsetWidth
-  const height = d3.select(id).node().offsetHeight
-
-  let margin = {
-    top: 15,
-    right: 15,
-    bottom: 20,
-    left: 60
+    this.render()
   }
-  const boundedWidth = width - margin.right - margin.left
-  const boundedHeight = height - margin.bottom - margin.top
 
-  console.log(33333, width, height, data)
+  render() {
+    this.wrapper = d3
+      .select(this.id)
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .attr('viewBox', [0, 0, this.width, this.height])
 
-  const yAccessor = (d) => d['temperatureMax']
-  const dateParser = d3.timeParse('%Y-%m-%d')
-  const xAccessor = (d) => dateParser(d['date'])
+    this.content = this.wrapper
+      .append('g')
+      .attr('transform', `translate(${this.width / 2}, ${this.height / 2})`)
 
-  // draw canvas
-  const wrapper = d3
-    .select(id)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .style('border', '1px solid')
+    this.path = this.content
+      .append('path')
+      .attr('d', this.radarLine(this.data))
+      .attr('fill', this.color)
+      .attr('fill-opacity', 0.2)
+      .attr('stroke', this.color)
+      .attr('stroke-width', 2)
+  }
 
-  const bounds = wrapper
-    .append('g')
-    .style('transform', `translate(${margin.left}px, ${margin.top}px)`)
+  radarLine(data) {
+    const maxValue = 10
+    const rScale = d3
+      .scaleLinear()
+      .domain([0, maxValue])
+      .range([0, this.height / 2])
+    const angleSlice = (Math.PI * 2) / data.length
 
-  // draw scales
-  const yScale = d3
-    .scaleLinear()
-    .domain(d3.extent(data, yAccessor))
-    .range([boundedHeight, 0])
-
-  const freezingTemperatures = bounds
-    .append('rect')
-    .attr('x', 0)
-    .attr('y', yScale(50))
-    .attr('width', boundedWidth)
-    .attr('height', boundedHeight - yScale(50))
-    .attr('fill', 'skyblue')
-
-  const xScale = d3
-    .scaleTime()
-    .domain(d3.extent(data, xAccessor))
-    .range([0, boundedWidth])
-
-  // draw data
-  const lineGenerator = d3
-    .line()
-    .x((d) => xScale(xAccessor(d)))
-    .y((d) => yScale(yAccessor(d)))
-
-  const line = bounds
-    .append('path')
-    .attr('d', lineGenerator(data))
-    .attr('fill', 'none')
-    .attr('stroke', 'green')
-    .attr('stroke-width', 2)
-
-  // draw peripherals
-  const yAxisGenerator = d3.axisLeft().scale(yScale)
-  const yAxis = bounds.append('g').call(yAxisGenerator)
-
-  const xAxisGenerator = d3.axisBottom().scale(xScale)
-  const xAxis = bounds
-    .append('g')
-    .call(xAxisGenerator)
-    .style('transform', `translate(0, ${boundedHeight}px)`)
+    return d3
+      .lineRadial()
+      .curve(d3.curveCardinalClosed)
+      .radius((d) => rScale(d))
+      .angle((d, i) => i * angleSlice)(data)
+  }
 }
 
-createLineChart()
+const blob = new Blob('#blob', '#08BDBA')
