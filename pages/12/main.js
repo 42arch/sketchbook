0e861872b8356fccc0c602f2a289f8da95e6d94a
42arch <rend42@163.com>
2023-11-07 16:29:38 +0800

    feat: :art: update pixel board

--- pages/12/main.js
+++ pages/12/main.js
@@ -9,6 +9,7 @@ class PixelBoard {
     this.color = options.color || 'black'
     this.resolution = options.resolution || 10
     this.isDrawing = false
+    this.isErasing = false
     this.canvas = document.createElement('canvas')
     this.context = this.canvas.getContext('2d')
     this.init()
@@ -42,20 +43,42 @@ class PixelBoard {
     let y =
       Math.floor((e.clientY - rect.top) / this.resolution) * this.resolution
 
-    this.context.fillStyle = this.color
-    this.context.fillRect(x, y, this.resolution, this.resolution)
+    if (this.isErasing) {
+      this.context.clearRect(x, y, 10, 10)
+    } else {
+      this.context.fillStyle = this.color
+      this.context.fillRect(x, y, this.resolution, this.resolution)
+    }
+  }
+
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  saveAsImage() {
+    let imageData = this.canvas.toDataURL('image/png')
+    let image = new Image()
+    image.src = imageData
+    let link = document.createElement('a')
+    link.href = image.src
+    link.download = 'canvas_image.png'
+    link.click()
   }
 
   changeOptions(options) {
     this.color = options.color || 'black'
     this.resolution = options.resolution || 10
+    this.isErasing = options.eraseMode
   }
 }
 
 const gui = new GUI()
 const options = {
-  color: '#08BDBA',
-  resolution: 6
+  color: '#4d4c4c',
+  eraseMode: false,
+  resolution: 6,
+  clear() {},
+  saveAsImage() {}
 }
 
 const board = new PixelBoard('board', options)
@@ -68,4 +91,17 @@ gui.addColor(options, 'color', 0, 40, 1).onChange((v) => {
 gui.add(options, 'resolution', ['3', '6', '10', '12']).onChange((v) => {
   options.resolution = v
   board.changeOptions(options)
+})
+
+gui.add(options, 'eraseMode').onChange((v) => {
+  options.eraseMode = v
+  board.changeOptions(options)
+})
+
+gui.add(options, 'clear').onChange(() => {
+  board.clear()
+})
+
+gui.add(options, 'saveAsImage').onChange(() => {
+  board.saveAsImage()
 })
