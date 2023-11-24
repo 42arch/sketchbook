1d85b21348369d6d9ab2cf36bd9cd21b0350908b
42arch <rend42@163.com>
2023-11-24 20:36:50 +0800

    fix: update delaunay

--- pages/13/main.js
+++ pages/13/main.js
@@ -11,13 +11,8 @@ class Delaunay {
     this.canvas = null
     this.ctx = null
     this.path = null
-    this.delaunay = d3.Delaunay.from(this.data)
-    this.voronoi = this.delaunay.voronoi([
-      0.5,
-      0.5,
-      this.width - 0.5,
-      this.height - 0.5
-    ])
+    this.delaunay = null
+    this.voronoi = null
 
     this.render()
   }
@@ -28,28 +23,66 @@ class Delaunay {
       .append('canvas')
       .attr('width', this.width * this.dpi)
       .attr('height', this.height * this.dpi)
-      .style('width', `${this.width}px`)
-      .style('height', `${this.height}px`)
+    // .style('width', `${this.width}px`)
+    // .style('height', `${this.height}px`)
     this.ctx = this.canvas.node().getContext('2d')
     this.ctx.scale(this.dpi, this.dpi)
-    // this.path = d3.geoPath(null, this.ctx).pointRadius(2.5)
 
-    this.data.forEach((i) => {
-      this.ctx.strokeStyle = '#000'
+    this.delaunay = d3.Delaunay.from(this.data)
+    this.voronoi = this.delaunay.voronoi([
+      1,
+      1,
+      this.width - 1,
+      this.height - 1
+    ])
+
+    for (let i = 0; i < this.data.length; i++) {
+      // this.ctx.strokeStyle = '#000'
+      // this.voronoi.renderCell(i, this.ctx)
+      // this.ctx.stroke()
+
+      const v = [...this.voronoi.neighbors(i)]
+      const d = [...this.delaunay.neighbors(i)]
+      const u = d.filter((j) => !v.includes(j))
+      console.log('1111', u)
+
+      const cell = this.voronoi.cellPolygon(i)
+      this.ctx.fillStyle = '#fff'
       this.ctx.beginPath()
-      this.voronoi.render(this.ctx)
-      this.voronoi.renderBounds(this.ctx)
-
-      this.ctx.stroke()
-
-      this.ctx.fillStyle = '#000'
-      this.ctx.beginPath()
-      d3.geoPath(null, this.ctx).pointRadius(2.5)({
-        type: 'MultiPoint',
-        coordinates: this.data
-      })
+      this.ctx.moveTo(cell[0][0], cell[0][1])
+      for (let j = 1; j < cell.length; j++) {
+        this.ctx.lineTo(cell[j][0], cell[j][1])
+      }
+      this.ctx.closePath()
       this.ctx.fill()
-    })
+      this.ctx.strokeStyle = '#000'
+      this.ctx.stroke()
+      this.ctx.closePath()
+
+      const p = this.data[i]
+      this.ctx.beginPath()
+      this.ctx.arc(...p, 2, 2 * Math.PI, false)
+      this.ctx.fillStyle = '#000'
+      this.ctx.fill()
+      this.ctx.closePath()
+
+      this.ctx.canvas.ontouchmove = this.ctx.canvas.onmousemove = (e) => {
+        const h = this.delaunay.find(e.layerX, e.layerY)
+        console.log('move', h)
+
+        // const cell = this.voronoi.cellPolygon(h)
+        // this.ctx.fillStyle = '#040'
+        // this.ctx.beginPath()
+        // this.ctx.moveTo(cell[0][0], cell[0][1])
+        // for (let j = 1; j < cell.length; j++) {
+        //   this.ctx.lineTo(cell[j][0], cell[j][1])
+        // }
+        // this.ctx.closePath()
+        // this.ctx.fill()
+
+        // this.ctx.closePath()
+      }
+    }
   }
 }
 
